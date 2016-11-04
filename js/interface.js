eb812e12ba0cd9c98ff6a2678659eeb72b012157
Jazzy <jasmina.gasper@gmail.com>
2016-11-04 17:32:27 +0000

    floating nav

--- js/interface.js
+++ js/interface.js
@@ -1,3 +1,12 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  var top = $('#nav').offset().top - parseFloat($('#nav').css('marginTop').replace(/auto/, 100));
+  $(window).scroll(function (event) {
+    var y = $(this).scrollTop();
 
+    if (y >= top) {
+      $('#nav').addClass('fixed');
+    } else {
+      $('#nav').removeClass('fixed');
+    }
+  });
 });
